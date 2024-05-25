8d018b91357dbd1555fc8ecc08aa7dadb71a1689
vishantrathi <vishantrathigurjar@gmail.com>
2024-05-25 16:10:25 +0530

    refactor: Update JavaScript code to use let and const for account variables

--- Javascript/javascript1.js
+++ Javascript/javascript1.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+//console.log("Hello World")
+// This is a comment in Javascript
+const accountId = 1234;
+let accountEmail = "vishant@gmail.com";
+let accountPassword = "password";
+//accountCity = "Delhi";
+accountEmail = "vishant@email.com";
+accountPassword = "newpassword";
+accountCity = "Mumbai";
+
+//accountId =2
+console.log(accountId);

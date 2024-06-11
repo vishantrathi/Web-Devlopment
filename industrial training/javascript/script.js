c40f59ae28402bdc1c47218bff294532fe376f36
vishantrathi <vishantrathigurjar@gmail.com>
2024-06-11 10:02:10 +0530

    Update JavaScript and CSS files for industrial training

---
+++ industrial training/javascript/script.js
@@ -0,0 +1,7 @@
+document.write("Hello World to CGC!");
+console.log("Hello World!");
+document.getElementById("demo").innerHTML = "Hello World to COE!";
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z);

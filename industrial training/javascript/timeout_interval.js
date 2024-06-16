1670023c626d98ccd16f079ecae07718aa5c9ad6
vishantrathi <vishantrathigurjar@gmail.com>
2024-06-17 00:09:44 +0530

    explanations of var, let, and const

--- industrial training/javascript/timeout_interval.js
+++ industrial training/javascript/timeout_interval.js
@@ -21,3 +21,10 @@ setInterval(function(){
 function stopInterval(){
     clearInterval(interval);
 }
+// Q:Can you explain the difference between var, let, and const?
+
+// var: var is function scoped when it is declared within a function. This means that it is available and can be accessed only within that function.
+
+// let: let is block scoped. A block is a chunk of code bounded by {}. A block lives in curly braces. Anything declared with let is only available inside that block.
+
+// const: const is used to declare variables that won’t be reassigned. const is block scoped and works like let but the value of the variable can’t be changed.

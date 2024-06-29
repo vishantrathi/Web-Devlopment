da2aee831a5685ae10d7c7423d9035762fb88d9f
vishantrathi <vishantrathigurjar@gmail.com>
2024-06-29 22:14:47 +0530

    scope in js

--- Javascript/defined _not_defined.js
+++ Javascript/defined _not_defined.js
@@ -1,7 +1,22 @@
-console.log(a);//output:-undefined
+console.log(a);//output:-undefined  
 var a=7;
 console.log(a);
 
 if(a == undefined){
+    console.log("a is undefined")
     
 }
+else{
+    console.log("a is not undefined")
+}
+
+
+var a;
+console.log(a);
+a=10;
+console.log(a);
+a="a is now a string"
+console.log(a);
+//javascript is a loosely type language
+
+
---
+++ Javascript/scope_in_js.js
@@ -0,0 +1,11 @@
+function a(){
+    var b =10;
+    c();
+    function c(){
+        
+    }
+    console.log(b);//we can access inside a
+}
+var b=10;
+a();
+//scope means a sepecial variable and function in our code

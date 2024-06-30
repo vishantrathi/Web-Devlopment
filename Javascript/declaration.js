de30412ea15c32d4e4b0284c3705365ad655fa16
vishantrathi <vishantrathigurjar@gmail.com>
2024-06-30 13:05:03 +0530

    decleration in js

---
+++ Javascript/declaration.js
@@ -0,0 +1,7 @@
+
+let a=10;// let = re assign only + not re declare the same variable
+console.log(a);
+var b=20;// var = assign + declare both the same variable
+console.log(b);
+const c=30;//const =not re assign  + not re declare the same variable
+console.log(c);

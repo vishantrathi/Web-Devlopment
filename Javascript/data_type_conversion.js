36639c18e5af8ba25b9c805af8017d4eacb7cfa4
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-04 21:45:38 +0530

    data type convertion

--- Javascript/data_type_conversion.js
+++ Javascript/data_type_conversion.js
@@ -1,5 +1,6 @@
-let score =33
+let score =22
 console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof score)
+console.log(typeof valueInNumber)
+//convertion of string not possible

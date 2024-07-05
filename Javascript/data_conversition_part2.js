40893d36d39e30ed13d63f23fc03a057dbf9b867
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-05 08:27:02 +0530

    data_conversition part 2

---
+++ Javascript/data_conversition_part2.js
@@ -0,0 +1,15 @@
+// let score = "33abc"// not a pure number
+// let score ="33"
+let score =NaN;
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber); //output=NaN
+
+
+//33 => 33
+//33abc => NaN
+//True => 1; false =>0
+

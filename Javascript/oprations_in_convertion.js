80970ec0b4700ab10775c8ed678b28e026887324
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-05 08:54:27 +0530

    comparison

--- Javascript/oprations_in_convertion.js
+++ Javascript/oprations_in_convertion.js
@@ -13,3 +13,9 @@ let str1="hello"
 let str2="world"
 let str3=str1+str2
 console.log(str3);
+
+console.log("1"+2); //agar phele string hai to sabko string mai treat kia jaye ga
+console.log(1+"2");
+console.log(1 + 1 +"2");// agar last mai string hai toh to phele convertion hoga baad mai string add hogi
+console.log("1"+1+2);
+console.log(1*1/2%2);

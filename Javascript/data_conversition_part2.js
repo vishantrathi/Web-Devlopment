fb4116dbbf8cbfed16d0c978dbe674c1f1ac8c6b
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-05 08:54:02 +0530

    oprations

--- Javascript/data_conversition_part2.js
+++ Javascript/data_conversition_part2.js
@@ -13,3 +13,15 @@ console.log(valueInNumber); //output=NaN
 //33abc => NaN
 //True => 1; false =>0
 
+let isLoggedIn ="string"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true ; 0=>false
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+console.log(somenumber);
+console.log(typeof somenumber);
+
+

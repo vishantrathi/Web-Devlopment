1e5228efde8a9c9659f7f51c4699945a2de41bad
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-06 21:45:12 +0530

    comparison of data types

--- Javascript/comparison of data type.js
+++ Javascript/comparison of data type.js
@@ -1,1 +1,12 @@
-console
+// numeric string used with + gives string type
+let result;
+
+// convert number to string
+result = "3" + 2; 
+console.log(result, "-", typeof(result));
+
+result = "3" + true; 
+console.log(result, "-", typeof(result));
+
+result = "3" + null; 
+console.log(result, "-", typeof(result));

b1343824c39b9c7d593c924a962b47aa451e4803
vishantrathi <vishantrathigurjar@gmail.com>
2024-07-11 21:47:37 +0530

    data types in Js

--- Javascript/comparison of data type.js
+++ Javascript/comparison of data type.js
@@ -8,7 +8,11 @@ console.log(result, "-", typeof(result));
 result = "3" + true; 
 console.log(result, "-", typeof(result));
 
-result = "3" + null; 
-console.log(result, "-", typeof(result));
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null == 0);
 
-
+//comparision and equallity check both are different in js
+//strict check === ( does not convert the data type)
+console.log("2"===2)
---
+++ Javascript/data_type_in_js.js
@@ -0,0 +1,10 @@
+//There are two types of data types are
+//Primitive
+//Primitive have 7 types:String,Number,Boolean,null,undefined,symbol,BigInt(scientific Values)
+
+
+//Non-Primitive
+//Type:Array,Object,Function
+
+
+//JS is a dynamically typed

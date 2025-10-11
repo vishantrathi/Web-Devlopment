08885e49aab71faec4422e40c556aea55470cd33
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2025-10-11 12:06:11 +0530

    commit

---
+++ JavaScript/Variables.js
@@ -0,0 +1,49 @@
+// console.log("Hey this is javascript")
+// var a=5;
+// var b=5;
+// var c="Hello";
+// console.log(a+b+8)
+// console.log(typeof a,typeof b,typeof c)
+// // Nameing convention
+// // 1.Letters, digits, underscores and $ sign are allowed
+// // 2. must begin with a,$,_or a Letters
+// // 3. hello and Hello are different variable
+
+// // let vs var in js
+// //var is globally scoped while let and const are blocked scoped
+// //var can be updated but  re-declared with in its acope
+// //let can be update nor be re-declared
+// //const can neither be updated nor be re-declared
+// //var can be initialized with ubdefined whereas let and const variable are not initialized
+// //const must be initalized during declaration unlike let and var
+
+
+// //Primitive Data types in  js 
+// //Null
+// //Number
+// //String 
+// //Symbol
+// //Undefined
+// //Boolean
+// //BigInt
+
+// let x="hello";
+// let y=22;
+// let z=33.3;
+// const d=true;
+// let q= undefined;
+// let r= null;
+// console.log(x,y,z,d,q,r)
+// console.log(typeof x,typeof y,typeof z,typeof d,typeof q,typeof r )
+// //Object
+
+// const item={
+//     name:"Javascript",
+//     price:"80"
+// }
+// console.log(item)
+
+// item.price="100";
+// console.log(item);
+
+const accountId=

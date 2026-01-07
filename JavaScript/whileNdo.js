a18ae1977c7ac89d6688f4646b47d2db792e2fdf
vishantrathi <129350236+vishantrathi@users.noreply.github.com>
2026-01-07 23:20:23 +0530

    commit

---
+++ JavaScript/whileNdo.js
@@ -0,0 +1,28 @@
+// while (condition) {
+    
+// }
+
+
+// do {
+    
+// } while (condition);
+
+let index =0
+while(index <=10){
+    console.log(`While Loop Value ${index}`)
+    index++
+}
+
+let myArray=[10,20,30,40,50]
+let arr=0
+while(arr < myArray.length){
+    console.log(`Value of ${myArray[arr]}`)
+    arr++
+}
+
+//do while loop
+let count=0
+do{
+    console.log(`Do While Loop Value ${count}`)
+    count++
+}while(count <= 10)
